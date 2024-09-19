290dea7bb85f5ebf55b0b305586bcf1ddf1fceed
Pranj99 <barvep@oregonstate.edu>
2024-09-18 19:32:49 -0700

    added posts and right side trending

--- SocialMedia-Starter/src/pages/home/Home.jsx
+++ SocialMedia-Starter/src/pages/home/Home.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import './Home.css'
 import ProfileSide from '../../components/profilesSide/ProfileSide'
 import PostSide from '../../components/PostSide/PostSide'
+import RightSide from '../../components/Rightside/RightSide'
 
 const Home = () => {
   return (
     <div className="Home">
         <ProfileSide></ProfileSide>
         <PostSide/>
-
-
-        <div className="RightSide">RightSide</div>    
+        <RightSide/>
     </div>
   )
 }
